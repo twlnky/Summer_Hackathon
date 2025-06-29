0ae0b7822b2748c3ea04acdc78d4536054f539dc
n1p <107926733+twlnky@users.noreply.github.com>
2025-06-30 00:57:35 +0300

    Jira test

--- src/main/java/rut/miit/tech/summer_hackathon/SummerHackathonApplication.java
+++ src/main/java/rut/miit/tech/summer_hackathon/SummerHackathonApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SummerHackathonApplication {
+    //TODO: SomeLogic
 
     public static void main(String[] args) {
         SpringApplication.run(SummerHackathonApplication.class, args);
