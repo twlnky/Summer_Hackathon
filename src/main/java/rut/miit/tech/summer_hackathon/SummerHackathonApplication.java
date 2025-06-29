0274500c01cf3aed9b05fa24c786df7da3a5138b
n1p <107926733+twlnky@users.noreply.github.com>
2025-06-30 00:19:49 +0300

    initial commit

---
+++ src/main/java/rut/miit/tech/summer_hackathon/SummerHackathonApplication.java
@@ -0,0 +1,13 @@
+package rut.miit.tech.summer_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rut/miit/tech/summer_hackathon/SummerHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package rut.miit.tech.summer_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
